80de7b18292bc9a43a766470718a02427f33485d
Sylvain Filoni <fffiloni@users.noreply.huggingface.co>
2024-09-01 11:00:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -272,15 +272,16 @@ def infer(ref_style_file, style_description, caption):
         # Reset the state after inference, regardless of success or failure
         reset_inference_state()
 
-def transform(image):
+def transform(tensor):
     """
     Define the necessary transformations for the image.
     """
+    to_pil = T.ToPILImage()
     return T.Compose([
         T.Resize((256, 256)),  # Example resize, adjust as needed
         T.ToTensor(),
         T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])(image)
+    ])(to_pil(tensor))
 
 def infer_compo(style_description, ref_style_file, caption, ref_sub_file):
     global models_rbm, models_b
