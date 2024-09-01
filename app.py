fccc571e598eaee894103193955abd417a5e4e12
Sylvain Filoni <fffiloni@users.noreply.huggingface.co>
2024-09-01 12:15:11 +0000

    Update app.py

--- app.py
+++ app.py
@@ -324,7 +324,7 @@ def infer_compo(style_description, ref_style_file, caption, ref_sub_file):
         
         # Convert tensor to PIL Image before passing to sam_model.predict
         x0_preview_pil = T.ToPILImage()(x0_preview[0])
-        sam_mask, boxes, phrases, logits = sam_model.predict(x0_preview_pil, sam_prompt)
+        sam_mask, boxes, phrases, logits = sam_model.predict(transform(x0_preview_pil), sam_prompt)
         sam_mask = sam_mask.detach().unsqueeze(dim=0).to(device)
         
         conditions = core.get_conditions(batch, models_rbm, extras, is_eval=True, is_unconditional=False, eval_image_embeds=True, eval_subject_style=True, eval_csd=False)
