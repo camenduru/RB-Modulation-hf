241d1e24069fd9965f41a4a973cf1801287a48f6
Sylvain Filoni <fffiloni@users.noreply.huggingface.co>
2024-09-02 12:42:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -251,8 +251,10 @@ def infer_compo(style_description, ref_style_file, caption, ref_sub_file):
         ## SAM Mask for sub
         use_sam_mask = False
         x0_preview = models_rbm.previewer(x0_forward)
-        
-        sam_mask, boxes, phrases, logits = sam_model.predict(transform(x0_preview[0]), sam_prompt)
+
+        x0_preview_pil = T.ToPILImage()(x0_preview[0].cpu())
+        sam_mask, boxes, phrases, logits = sam_model.predict(x0_preview_pil, sam_prompt)
+        # sam_mask, boxes, phrases, logits = sam_model.predict(transform(x0_preview[0]), sam_prompt)
         sam_mask = sam_mask.detach().unsqueeze(dim=0).to(device)
         
         conditions = core.get_conditions(batch, models_rbm, extras, is_eval=True, is_unconditional=False, eval_image_embeds=True, eval_subject_style=True, eval_csd=False)
